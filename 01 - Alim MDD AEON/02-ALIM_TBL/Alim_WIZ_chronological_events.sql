9b2fd51d371a5b06a28339192acc15292410481c
antoine larcher <44400727+dr0ant@users.noreply.github.com>
2022-10-30 21:30:29 +0100

    xx

--- 01 - Alim MDD AEON/02-ALIM_TBL/Alim_WIZ_chronological_events.sql
+++ 01 - Alim MDD AEON/02-ALIM_TBL/Alim_WIZ_chronological_events.sql
@@ -1,13 +1,13 @@
-create function Alim_WIZ_chronological_events() returns varchar
+create function "WizeCosm".Alim_WIZ_chronological_events() returns varchar
     language plpgsql
 as
 $$
 begin
     DELETE
-    FROM wiz_chronological_events
+    FROM "WizeCosm".wiz_chronological_events
     WHERE 1 = 1;
 
-    INSERT INTO wiz_chronological_events(Type, Label, OBJ_ID, chronological_position, Parent_OBJ_ID, start_date,
+    INSERT INTO "WizeCosm".wiz_chronological_events(Type, Label, OBJ_ID, chronological_position, Parent_OBJ_ID, start_date,
                                          duration,
                                          end_date, Blocked_BY, Blocks, Evernote_LINk, location, ADDED_DATE)
     SELECT src."Type",
@@ -23,11 +23,11 @@ begin
            src."Links",
            src."Location",
            NOW()
-    FROM "IMP_aeon_data" src
+    FROM "WizeCosm"."IMP_aeon" src
     WHERE 1 = 1
       AND src."Type" IN ('Periode', 'Arc', 'Ere', 'Backstory', 'Flashback', 'Event');
     RETURN 'OK';
-END;
+END
 $$;
 
 
---
+++ 01 - Alim MDD AEON/02-ALIM_TBL/Alim_WIZ_personnages.sql
@@ -0,0 +1,46 @@
+create function "WizeCosm".alim_wiz_personnages() returns character varying
+    language plpgsql
+as
+$$
+begin
+    DELETE
+    FROM "WizeCosm"."WIZ_personnages"
+    WHERE 1 = 1;
+
+    INSERT INTO "WizeCosm"."WIZ_personnages" (
+                                         "character_type",
+                                         "character_label",
+                                         "character_identifier",
+                                         "character_parent",
+                                         "character_links",
+                                         "character_relates_to",
+                                         "character_character_type",
+                                         "character_gender",
+                                         "character_race",
+                                         "character_faction",
+                                         "character_goals",
+                                         "character_nickname",
+                                         added_date)
+    SELECT src."Type",
+           src."Label",
+           src."Identifier",
+           src."Parent",
+           src."Links",
+           src."Relates_to",
+           src."Character_Type",
+           src."Gender",
+           src."Race",
+           src."Faction",
+           src."Goals",
+           src."Nickname",
+           NOW()
+    FROM "WizeCosm"."IMP_aeon" src
+    WHERE 1 = 1
+      AND src."Type" ='Character'
+    ;
+    RETURN 'OK';
+END
+$$;
+
+
+
