1e35aa14c1cf695b8c2a4672d0414d16aa3e7902
antoine larcher <44400727+dr0ant@users.noreply.github.com>
2023-11-10 14:15:54 +0100

    Update Analyses of tikers.sql

--- WizeCosm/WizeTrading/Analyses of tikers.sql
+++ WizeCosm/WizeTrading/Analyses of tikers.sql
@@ -67,3 +67,79 @@ select *
 from wizetrading.combined_stream_treated
 where stream ='btcusd_perp@kline_5m'
     );
+
+create view wizetrading.v_BTCUSD_PERP_15min
+as
+(
+select *
+from wizetrading.combined_stream_treated
+where stream ='btcusd_perp@kline_15m'
+    );
+
+create view wizetrading.v_BTCUSD_PERP_1H
+as
+(
+select *
+from wizetrading.combined_stream_treated
+where stream ='btcusd_perp@kline_1h'
+    );
+
+create view wizetrading.v_BTCUSD_PERP_4H
+as
+(
+select *
+from wizetrading.combined_stream_treated
+where stream ='btcusd_perp@kline_4h'
+    );
+
+create view wizetrading.v_BTCUSD_PERP_1D
+as
+(
+select *
+from wizetrading.combined_stream_treated
+where stream ='btcusd_perp@kline_1d'
+    );
+
+create view wizetrading.v_BTCUSD_PERP_1W
+as
+(
+select *
+from wizetrading.combined_stream_treated
+where stream ='btcusd_perp@kline_1w'
+    );
+
+------------------------------------------------------------------------
+create view wizetrading.v_combined_treated_streams
+as
+(
+select
+/*indicators*/
+
+
+/*source data*/
+five_min.*,
+fifteen_min.*,
+one_hour.*,
+four_hour.*,
+one_day.*,
+one_week.*
+from wizetrading.v_btcusd_perp_5min as five_min
+/* link 15 min chart */
+         left join wizetrading.v_btcusd_perp_15min as fifteen_min
+                   ON five_min.event_time = fifteen_min.event_time
+/* link 1h  chart */
+         left join wizetrading.v_btcusd_perp_1h as one_hour
+                   ON five_min.event_time = one_hour.event_time
+/* link 4h  chart */
+         left join wizetrading.v_btcusd_perp_4h as four_hour
+                   ON five_min.event_time = four_hour.event_time
+/* link 1d chart */
+         left join wizetrading.v_btcusd_perp_1d as one_day
+                   ON five_min.event_time = one_day.event_time
+/* link 1w chart */
+         left join wizetrading.v_btcusd_perp_1w as one_week
+                   ON five_min.event_time = one_week.event_time
+    )
+;
+
+
