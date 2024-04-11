e12a1e3a8fc941e9346551f338fb30631cc3cc5d
antoine larcher <44400727+dr0ant@users.noreply.github.com>
2024-04-11 19:15:13 +0200

    add track spends

--- WizeSpends/Spends_analysis : overspending.sql
+++ WizeSpends/Spends_analysis : overspending.sql
@@ -114,10 +114,11 @@ where date  between  '2024-01-01':: date and '2025-04-27':: date
 drop function if exists wizespends.did_I_overSpend(actual_amount_param int);
 CREATE OR REPLACE FUNCTION wizespends.did_I_overSpend(actual_amount_param int)
 RETURNS TABLE (
-    date_today date,
+    _date_today date,
     actual_amount int,
     suposed_amount float,
-    over_spent_amount varchar
+    over_spent_amount varchar,
+    dailyspend_actualized float
 ) AS
 $$
 DECLARE
@@ -132,12 +133,14 @@ BEGIN
         CASE
             WHEN rest_to_use < actual_amount_param THEN 'UNDERSPEND ' || TO_CHAR(actual_amount_param - rest_to_use, '9999999')
             WHEN rest_to_use > actual_amount_param THEN 'OVERSPEND ' || TO_CHAR(actual_amount_param - rest_to_use, '9999999')
-        END AS over_spent_amount
+        END AS over_spent_amount,
+        actual_amount/nb_of_day_last as dailyspend_actualized
     INTO
-        date_today,
+        _date_today,
         actual_amount,
         suposed_amount,
-        over_spent_amount
+        over_spent_amount,
+        dailyspend_actualized
 
     FROM
         wizespends.overspend_underspend
@@ -152,3 +155,81 @@ LANGUAGE plpgsql;
 
 SELECT * FROM wizespends.did_I_overSpend(1239);
 
+
+-----------------------------------
+-- table
+DROP TABLE IF EXISTS wizespends.spending_summary;
+CREATE TABLE IF NOT EXISTS wizespends.spending_summary (
+    _date_today DATE,
+    actual_amount INT,
+    suposed_amount FLOAT,
+    over_spent_amount VARCHAR,
+    dailyspend_actualized FLOAT
+);
+ALTER TABLE wizespends.spending_summary
+ADD CONSTRAINT spending_summary_unique_date UNIQUE (_date);
+
+
+--stored procedure
+DROP PROCEDURE IF EXISTS update_spending_summary(current_amount_param INT);
+CREATE OR REPLACE PROCEDURE update_spending_summary(current_amount_param INT)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    _date_today DATE;
+BEGIN
+    -- Call the view to get spending details
+    drop table if exists spends;
+
+    CREATE TEMPORARY TABLE spends AS
+    SELECT
+        _date,
+        current_amount_param AS actual_amount,
+        rest_to_use AS suposed_amount,
+        CASE
+            WHEN rest_to_use < current_amount_param THEN 'UNDERSPEND ' || TO_CHAR(current_amount_param - rest_to_use, '9999999')
+            WHEN rest_to_use > current_amount_param THEN 'OVERSPEND ' || TO_CHAR(current_amount_param - rest_to_use, '9999999')
+            WHEN rest_to_use = current_amount_param THEN 'ON TRACK PERFECT ' || TO_CHAR(current_amount_param - rest_to_use, '9999999')
+        END AS over_spent_amount,
+        (current_amount_param * 1.00) / nb_of_day_last AS updated_daily_spend
+    FROM
+        wizespends.overspend_underspend
+    WHERE
+        _date = CURRENT_DATE;
+
+    -- Merge the data into the spending_summary table
+    INSERT INTO wizespends.spending_summary (_date, actual_amount, suposed_amount, over_spent_amount, dailyspend_actualized)
+    SELECT
+        _date,
+        actual_amount,
+        suposed_amount,
+        over_spent_amount,
+        updated_daily_spend
+    FROM
+        spends
+    ON CONFLICT (_date) DO UPDATE SET
+        actual_amount = EXCLUDED.actual_amount,
+        suposed_amount = EXCLUDED.suposed_amount,
+        over_spent_amount = EXCLUDED.over_spent_amount,
+        dailyspend_actualized = EXCLUDED.dailyspend_actualized;
+
+drop table if exists spends;
+
+END;
+$$;
+CALL update_spending_summary(600);
+
+
+/*
+select _date,
+          700 AS actual_amount,
+          rest_to_use as suposed_amount,
+        CASE
+            WHEN rest_to_use < 700 THEN 'UNDERSPEND ' || TO_CHAR(700 - rest_to_use, '9999999')
+            WHEN rest_to_use > 700 THEN 'OVERSPEND ' || TO_CHAR(700 - rest_to_use, '9999999')
+            WHEN rest_to_use = 700 THEN 'ON TRACK PERFECT ' || TO_CHAR(700 - rest_to_use, '9999999')
+        END AS over_spent_amount,
+            (700 * 1.00) / nb_of_day_last as updated_daily_spend
+     from wizespends.overspend_underspend
+     where _date = CURRENT_DATE
+ */
